617cad71dc499da5b6fc32fdc329c1e1ba5e3a71
jonjonathanarias <jonjonathanarias1@gmail.com>
2022-11-13 01:45:44 -0300

    Acomodando Archivo JS

--- Parking Lot Web/vista/admin/js/main.js
+++ Parking Lot Web/vista/admin/js/main.js
@@ -13,12 +13,40 @@ cerrarSesion.addEventListener('click', function(i){
  
 //Funcion Para El Boton De Borrar
 const botonBorrar =document.querySelectorAll(".btn-danger")
-const Contenido =document.getElementById('col3')
+
 
 function Borrar(){
     botonBorrar.preventDefault(confirm('¿Esta Seguro Que Desea Borrar?'))
+
     
 };
-    
+
+
+// Para la Tabla de Clientes
+
+const opcionesDeTabla={
+    pageLength:4,
+    lengthMenu: [2, 4, 8, 12],
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    },
+    columnDefs:[{className:"centrado", targets:[0, 1, 2, 3, 4]}],
+}
+
+$(document).ready( function () {
+    $('#tablaCliente').DataTable(opcionesDeTabla);
+} );
     
  
